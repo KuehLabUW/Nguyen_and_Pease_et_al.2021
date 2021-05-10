b1434a9955bc84a8cebabecfc5ea71f530143beb
KuehLabUW <43481207+KuehLabUW@users.noreply.github.com>
2021-05-10 13:36:51 -0700

    Add files via upload

---
+++ figure 2/positive feedback/plot_S_sweep.m
@@ -0,0 +1,29 @@
+% plotting activation time as a function of signal strength
+
+
+S = [25, 12, 8, 4, 2, 1.5, 1.0, 0.9, 0.85, 0.80, 0.75];
+t_median = [];
+t_q1 = [];
+t_q3= [];
+
+for i = 1:length(S)
+    filename = ['ttimes_' num2str(S(i)) '.csv'];
+    data = readtable(filename,'ReadVariableNames',false);
+    
+    d = data.Var1;
+    d(find(d==-1)) = Inf;
+    t_median = [t_median median(d)];
+    t_q1 = [t_q1 quantile(d, 0.25)];
+    t_q3 = [t_q3 quantile(d, 0.75)];
+end
+
+figure(1); subplot(1,2,1)
+errorbar(S, t_median, t_median - t_q1, t_q3 - t_median,'o-','MarkerSize', 9);
+set(gca,'YScale','log');
+set(gca,'XScale','log');
+set(gca, 'XLim',[0.1 25]);
+set(gca, 'YLim',[3 1000]);
+hold on;
+plot([0.7 0.7], [0.1 10000],'k-');
+plot([1 1], [0.1 10000],'k:');
+axis square
