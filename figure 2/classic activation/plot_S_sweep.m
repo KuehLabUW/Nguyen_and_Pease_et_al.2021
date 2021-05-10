a164f6ce6d22829a53246da1396c63ba8a5d896a
KuehLabUW <43481207+KuehLabUW@users.noreply.github.com>
2021-05-10 13:27:51 -0700

    Add files via upload

---
+++ figure 2/classic activation/plot_S_sweep.m
@@ -0,0 +1,29 @@
+% plotting activation time as a function of signal strength
+
+
+S = [120 125 130 135 140 145 150 160 180 200 400 800];
+t_median = [];
+t_q1 = [];
+t_q3= [];
+
+for i = 1:length(S)
+    filename = ['ttimes_' num2str(S(i)) '.csv'];
+    data = readtable(filename,'ReadVariableNames',false);
+    
+    d = data.Var1;
+    d(find(d==-1)) = Inf;
+    t_median = [t_median median(d)];
+    t_q1 = [t_q1 quantile(d, 0.25)];
+    t_q3 = [t_q3 quantile(d, 0.75)];
+end
+
+figure(1); subplot(1,2,1)
+errorbar(S, t_median, t_median - t_q1, t_q3 - t_median,'o-','MarkerSize',9);
+set(gca,'YScale','log');
+set(gca,'XScale','log');
+set(gca, 'XLim',[50 1000]);
+set(gca, 'YLim',[3 1000]);
+hold on;
+plot([125 125], [0.1 10000],'k-');
+plot([150 150], [0.1 10000],'k:');
+axis square
